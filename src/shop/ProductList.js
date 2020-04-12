91022279a363aa8df29efe31033dee99b54bdab4
KhacNgoc <nguyenngoc99uet@gmail.com>
2020-04-12 15:30:11 +0700

    Shop.

--- src/shop/ProductList.js
+++ src/shop/ProductList.js
@@ -5,7 +5,7 @@ export class ProductList extends Component {
             return <h5 className="p-2">No Products.</h5>
         }
         return this.props.products.map(p => 
-            <div className="card m-1 p-1 bg-light" key="{p.id}">
+            <div className="card m-1 p-1 bg-light" key={p.id}>
                 <h4>
                     { p.name }
                     <span className="badge badge-pill badge-primary float-right">
